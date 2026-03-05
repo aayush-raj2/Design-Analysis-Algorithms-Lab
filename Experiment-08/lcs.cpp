2d4d0122067c85ff77fa665fc6a75580a3280cf7
Aayush Raj <aayushr679@gmail.com>
2026-03-05 21:26:58 +0530

    Implement LCS algorithm in lcs.cpp

---
+++ Experiment-08/lcs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    string X = "ABCDGH";
+    string Y = "AEDFHR";
+
+    int n = X.length();
+    int m = Y.length();
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+
+            if(X[i-1] == Y[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    cout << "Length of LCS: " << dp[n][m] << endl;
+
+    return 0;
+}
