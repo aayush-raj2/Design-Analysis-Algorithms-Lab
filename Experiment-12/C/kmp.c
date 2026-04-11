da4426d91bfc4928d962d6bb558f7a959ef41afa
Aayush Raj <aayushr679@gmail.com>
2026-04-11 21:26:06 +0530

    Create kmp.c

---
+++ Experiment-12/C/kmp.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+void computeLPS(char *pat, int m, int *lps) {
+    int len = 0;
+    lps[0] = 0;
+
+    int i = 1;
+    while (i < m) {
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0)
+                len = lps[len - 1];
+            else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+void KMP(char *text, char *pat) {
+    int n = strlen(text);
+    int m = strlen(pat);
+
+    int lps[m];
+    computeLPS(pat, m, lps);
+
+    int i = 0, j = 0;
+
+    while (i < n) {
+        if (text[i] == pat[j]) {
+            i++;
+            j++;
+        }
+
+        if (j == m) {
+            printf("Pattern found at index %d\n", i - j);
+            j = lps[j - 1];
+        }
+        else if (i < n && text[i] != pat[j]) {
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i++;
+        }
+    }
+}
+
+int main() {
+    char text[] = "ABABDABACDABABCABAB";
+    char pattern[] = "ABABCABAB";
+
+    KMP(text, pattern);
+
+    return 0;
+}
